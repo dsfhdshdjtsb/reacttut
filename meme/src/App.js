ca55573726fb1e7250ccb91748f7111e572f83ce
Nicholas Suh <nicksuh17@gmail.com>
2023-07-26 22:09:42 -0700

    some more progress and shit

---
+++ meme/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Meme from "./components/Meme";
+
+
+export default function App(){
+
+    return(
+        <div>
+            <Navbar />
+            <Meme />
+        </div>
+    )
+}
