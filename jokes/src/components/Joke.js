5b9691594af14dc0c71440f440a3fa70075d6db7
Nicholas Suh <nicksuh17@gmail.com>
2023-07-28 13:06:14 -0700

    meme

--- jokes/src/components/Joke.js
+++ jokes/src/components/Joke.js
@@ -1,8 +1,11 @@
+import React from "react"
 export default function Joke(props){
+    const [isShown, setIsShown] = React.useState(false)
     return(
             <div>
-                <p>{props.setup}</p>
-                <p>{props.punchline}</p>
+                {isShown && <p>{props.setup}</p>}
+                {isShown && <p>{props.punchline}</p>}
+                <button onClick={()=>{setIsShown(prev=>!prev)}}>{isShown ? "Hide" : "Show"} joke</button>
             </div>
         )
 }
--- meme/src/components/Meme.js
+++ meme/src/components/Meme.js
@@ -1,18 +1,64 @@
 import {Button, Form} from "react-bootstrap"
 import data from "../data"
+import React from "react"
+
 export default function Meme(){
-    function handleClick()
+    const [allMemeImages, setAllMemeImages] = React.useState([])
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        botText: "",
+        imgUrl: "https://i.imgflip.com/1big.jpg"
+    })
+    React.useEffect(() =>{
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response =>{
+            return response.json();
+        })
+        .then(memes=>{
+            setAllMemeImages(memes)
+        })
+    }, [])
+    function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prev =>{
+            return{
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+    console.log(meme.topText)
+    console.log(meme.botText)
+    function getImgUrl()
     {
-        return data.data.memes[Math.floor(Math.random() * 100)].url
+        const memesArray = allMemeImages.data.memes
+        return memesArray[Math.floor(Math.random() * 100)].url
     }
     return(
         <div className="Meme">
             <form>
             <div className="FormInput">
-                <Form.Control id="topText" type="text" placeholder="Top Text" />
-                <Form.Control id="botText" type="text" placeholder="Bottom Text" />
+                <input type="text"
+                    placeholder="Top Text"
+                    id="topText"
+                    name="topText"
+                    onChange={handleChange}
+                />
+                <input type="text"
+                    placeholder="Bottom Text"
+                    id="botText"
+                    name="botText"
+                    onChange={handleChange}
+                />
             </div>
-                <Button id="submit" onClick={() => console.log(handleClick())}>Generate my Meme</Button>
+                <Button id="submit" onClick={() => setMeme({...meme, imgUrl:getImgUrl()})}>Generate my Meme</Button>
+                <div className="meme">
+                    <img src={meme.imgUrl} className="meme--image"></img>
+                    <h2 className="meme--text top">{meme.topText}</h2>
+                    <h2 className="meme--text bottom">{meme.botText}</h2>
+                </div>
+                
             </form>
         </div>
     )
